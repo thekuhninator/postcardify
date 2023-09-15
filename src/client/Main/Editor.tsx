39530ce647a2ae5b951d988fa0b012048efb7efc
Roman Kuhn <romankuhn@microsoft.com>
2023-09-14 18:35:54 -0700

    Sets up routing. Styles home and editor page.

--- src/client/Main/Editor.tsx
+++ src/client/Main/Editor.tsx
@@ -1,17 +1,37 @@
-import React from 'react'
-import { useNavigate } from "react-router-dom";
+import "./Home.css";
+
+import { useState } from "react";
 import Header from './shared/Header';
+import reactLogo from "./assets/react.svg";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Jumbotron from "./Home/Jumbotron";
+import { Button, Card, Typography } from "@mui/material";
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 function Editor() {
-    return(
-        <div>
-            <Header />
-        <h1>
-        dis da foto editor HELLOO
-        </h1>
+  const [count, setCount] = useState(0);
 
-        </div>
-    );
+  return (
+
+    <div>
+            <Header />
+            <Container maxWidth="md" sx={{ marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+             }}>
+                <Card>
+                    <Typography variant="h3"> Free Online Postcard Maker </Typography>
+                    <Typography variant="subtitle1"> Take your photos from blah to beautiful with Postcardify's 
+                    free online photo editor. Upload, transform and share instantly from one place. </Typography>
+                    <Button variant="contained" startIcon={<CloudUploadIcon />}> Upload your image </Button>
+                    <p> or drop it here </p>
+                </Card>
+
+            </Container>
+        </div>  
+        );
 }
 
 export default Editor;
